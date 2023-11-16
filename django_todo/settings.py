fd793c5fc35962c3ece8f0f90a8cdcfd7d2b9316
vikdts <>
2023-11-16 10:48:42 +0000

    Fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-vikdts-hello-django-5141srnmb8.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['git.heroku.com/vbjk12-django-todo-app.git']
 
 
 # Application definition
